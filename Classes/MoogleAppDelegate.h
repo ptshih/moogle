2e39cac6539963c8146e549b8faf2b64ad6c35e6
Peter Shih <ptshih@gmail.com>
2011-01-31 19:47:22 -0800

    first commit

---
+++ Classes/MoogleAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  MoogleAppDelegate.h
+//  Moogle
+//
+//  Created by Peter Shih on 1/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoogleAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
