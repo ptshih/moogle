cbbe1db34ad00abcf908c8b193cc2115bc74634d
Peter Shih <ptshih@gmail.com>
2011-02-25 02:49:21 -0800

    added feedcell stub

---
+++ Classes/Views/FeedCell.h
@@ -0,0 +1,16 @@
+//
+//  FeedCell.h
+//  Moogle
+//
+//  Created by Peter Shih on 2/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MoogleCell.h"
+
+@interface FeedCell : MoogleCell {
+    
+}
+
+@end
